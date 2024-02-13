880b5ad6f40e47d4e1b59b3f97b2e31a7d105029
Juliette Landman <Juliettelandman@gmail.com>
2024-02-13 16:30:22 +0100

    Added player movement

--- HelloGXP(1)(1)/HelloGXP/GXPEngine/Player.cs
+++ HelloGXP(1)(1)/HelloGXP/GXPEngine/Player.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GXPEngine
-{
-    internal class Player
-    {
-        Player() : base() { }
+using GXPEngine;
 
+internal class Player : AnimationSprite
+{
+    float speed;
+    public Player() : base("barry.png",7,1) 
+    {
+        speed = 2f;
     }
-}
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(Key.LEFT))
+        { Move(-speed, 0); }
+        if (Input.GetKey(Key.RIGHT))
+        { Move(speed, 0); }
+        if (Input.GetKey(Key.UP))
+        { Move(0, -speed); }
+        if (Input.GetKey(Key.DOWN))
+        { Move(0, speed); }
+    }
+}   
+
