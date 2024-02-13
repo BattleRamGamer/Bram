891e19f9dc400e0b0251d9e2bda8add9178c2085
Bram <bram.salomons@gmail.com>
2024-02-13 17:28:02 +0100

    Enemy script added
    still gotta figure out how to make ShootingEnemy have its own sprite

---
+++ HelloGXP(1)(1)/HelloGXP/GXPEngine/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Enemy : Sprite
+{
+    
+
+    public Enemy() : base("colors.png")
+    {
+        
+        SetOrigin(width / 2, height / 2);
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+}
